1e5ebcca1c3b9bf23e8fcd22763163c65549f175
Simon Heglasik <38507657+simonheglasik@users.noreply.github.com>
2019-10-31 18:14:30 +0100

    03.09

---
+++ Openlab-03.09/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenLab_03._09
+{
+    class Class1
+    {
+        public bool IsPrimeNubmer(int number)
+        {
+            return number % 2 != 0;
+        }
+    }
+}
---
+++ Openlab-03.09/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenLab_03._09;
+namespace OpenLab_03._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 PrNumber = new Class1();
+            bool num = PrNumber.IsPrimeNubmer(5);
+            Console.WriteLine(num);
+        }
+    }
+}
